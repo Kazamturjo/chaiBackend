cc85235fc3b0c41c77518ba948a39953e1d6265f
kazamTurjo <turjo.kazam.tj@gmail.com>
2024-03-25 11:48:57 +0600

    2nd commit

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     console.log('DB_NAME ',DB_NAME );
     
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`,{dbName:`${DB_NAME}`})
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
